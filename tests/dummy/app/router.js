3efbf9570c5ee4c6cd9d3933531967c8a8af70a0
Lars Bünting <lars.buenting@smacc.io>
2017-10-20 08:36:49 +0200

    update everything

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,8 +1,8 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 import googlePageView from './mixins/google-page-view';
 
-const Router = Ember.Router.extend(googlePageView, {
+const Router = EmberRouter.extend(googlePageView, {
   location: config.locationType,
   rootURL: config.rootURL
 });
